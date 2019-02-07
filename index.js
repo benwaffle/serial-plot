61398364f70819ac868cc98ea27f6cef9e8ae16e
Ben Iofel <ben@flow.io>
2019-02-07 18:59:29 -0500

    show serial messages in the browser

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
-const app = express()
 const fs = require('fs')
+const WebSocket = require('ws')
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+
+const app = express()
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', (ws) => {
+    let port = null
+    ws.on('message', (msg) => {
+        console.log(`browser sent ${msg}`)
+        const device = JSON.parse(msg).device
+        if (port)
+            port.close()
+        port = new SerialPort(device, { baudRate: 115200 })
+        const parser = port.pipe(new Readline())
+
+        const listener = (data) => {
+            ws.send(data, (err) => {
+                if (err) {
+                    console.error(err)
+                    parser.removeListener('data', listener)
+                }
+            })
+        }
+        parser.addListener('data', listener)
+        parser.on('error', () => parser.removeListener('data', listener))
+    })
+})
 
 app.use(express.static('public'))
 app.use('/node_modules', express.static('node_modules'))
